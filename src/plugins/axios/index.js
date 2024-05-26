656d89b3f960a2836400ce9a3856dd6d10fcea58
La Gia Huy <103893858+lagiahuy2001@users.noreply.github.com>
2024-05-26 10:12:24 +0700

    update
    update

--- src/plugins/axios/index.js
+++ src/plugins/axios/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import jwt from "../jwt";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.put["Content-Type"] = "application/json";
-axios.defaults.withCredentials = false;
+axios.defaults.withCredentials = true;
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL
 
 export default axios.create();
